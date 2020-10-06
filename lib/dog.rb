8df4b63992ba34b15776ec9824f15b4dba0738d7
Kimberly Baker <bakerk2010@gmail.com>
2020-10-06 16:49:33 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name = "Fido"
+  end
+ 
+end
+
+class Breed
+  
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed = "Beagle"
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name = "Beyonce"
+  end
+ 
+end
+
+class Person
+ 
+  def job=(job)
+    @job = job
+  end
+ 
+  def job
+    @job = "Singer"
+  end
+ 
+end
